954c26672edef3286f00c17fa2c0969f26a6f41b
root <root@ip-172-31-0-103.us-east-2.compute.internal>
2020-02-28 05:23:59 +0000

    added main.tf file

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-east-2"
+}
+
+
+resource "aws_instance" "Nike-Test"  {
+  ami = "ami-f4f4cf91"   # ubuntu 16.04
+  instance_type = "t2.micro"
+   
+  tags = {
+     name ="Nike-test"
+	}
+}
+
+
